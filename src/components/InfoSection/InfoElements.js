78fb38c06d80ba759477701d1350b97a78fda5af
Shravan Suresh <shravansuresh02@gmail.com>
2021-07-16 22:33:18 +0530

    Added missing # to InfoContainer hexcode

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const InfoContainer = styled.div`
     color: #fff;
-    background: ${({lightBg}) => ('lightBg') ? '#f9f9f9' : '010606'}
+    background: ${({lightBg}) => ('lightBg') ? '#f9f9f9' : '#010606'}
     ;
 
     @media screen and (max-width: 768px) {
